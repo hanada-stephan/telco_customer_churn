52ab01a2dd706065cbca604e98a03e8cd4c11973
hanada-stephan <stephan.hanada@hotmail.com>
2022-09-25 02:58:49 -0300

    Added utils

---
+++ utils/plotting.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+def plot_confusion_matrix(y_test, y_pred):
+    """Plot a confusion matrix
+    
+    Args:
+        y_test (pandas series, nparray): Target test set
+        y_pred (pandas series, nparray): Predicted targets
+
+    Returns:
+        Figure containing the confusion matrix
+    """    
+
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
